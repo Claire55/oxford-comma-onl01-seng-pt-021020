76cd9b287255e2dffa723d0dbd22171d4b7a0152
Claire Bullock <bullockcm13@gmail.com>
2020-02-18 15:50:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   if array.size == 1
     then array.join
+    
   elsif array.size == 2 
     then array.join("and")
+    
   else array.size == 3
     then array.join(",")
   end
